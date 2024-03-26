12fea8d1c416af736067c3b7a2d61e52f0a282ca
doramonz <whs3867@icloud.com>
2024-03-26 19:43:52 +0900

    [Chore] : 패키지 경로 수정

--- src/main/java/live/smoothing/auth/SmoothingApplication.java
+++ src/main/java/live/smoothing/auth/SmoothingApplication.java
@@ -1,4 +1,4 @@
-package live.smoothing.smoothing;
+package live.smoothing.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
