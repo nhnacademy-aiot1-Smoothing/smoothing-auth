b9e36af80f2081a24e3ada44c28c3be2f545c434
doramonz <whs3867@icloud.com>
2024-03-27 11:07:49 +0900

    [Feat] : Redis DB Connection Done.

---
+++ src/main/java/live/smoothing/auth/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package live.smoothing.auth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@RequiredArgsConstructor
+@Configuration
+public class RedisConfig {
+    private final RedisProperties redisProperties;
+    @Value("${redis.dbIndex}")
+    private Integer dbIndex;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        lettuceConnectionFactory.setDatabase(dbIndex);
+        return lettuceConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
