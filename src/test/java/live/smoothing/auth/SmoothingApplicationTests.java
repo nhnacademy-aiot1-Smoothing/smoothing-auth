f59ce0a544ddc9b1d8227d4edc8d0aacd9cbef2a
nakta-y <nakta.y.biz@gmail.com>
2024-03-28 00:19:36 +0900

    [Fix] : 테스트 패키지 경로 수정

--- src/test/java/live/smoothing/auth/SmoothingApplicationTests.java
+++ src/test/java/live/smoothing/auth/SmoothingApplicationTests.java
@@ -1,13 +1,11 @@
-package live.smoothing.smoothing;
+package live.smoothing.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SmoothingApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
