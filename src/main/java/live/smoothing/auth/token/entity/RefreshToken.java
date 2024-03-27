d51ebbaf8e54c32063e0cf6bfff05d119f2992d4
doramonz <whs3867@icloud.com>
2024-03-27 14:14:49 +0900

    [Feat] : RefreshToken 리포지토리 및 서비스 구현

---
+++ src/main/java/live/smoothing/auth/token/entity/RefreshToken.java
@@ -0,0 +1,11 @@
+package live.smoothing.auth.token.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RefreshToken {
+    private String userId;
+    private String refreshToken;
+}
