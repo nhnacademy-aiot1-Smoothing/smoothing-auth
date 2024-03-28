e23dcd04c352f48e71225a57ad2cab33775eae6f
haaazzi <haa.zzing@gmail.com>
2024-03-28 15:32:39 +0900

    [Feat] : AuthController test code logout 추가

--- src/test/java/live/smoothing/auth/controller/AuthControllerTest.java
+++ src/test/java/live/smoothing/auth/controller/AuthControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.eq;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -48,7 +49,8 @@ class AuthControllerTest {
     }
 
     @Test
-    void logout() {
-
+    void logout() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockMvc.perform(delete("/api/auth/logout").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(refreshToken)).header("X-USER-ID", userId)).andExpect(status().isOk()).andDo(print());
     }
 }
