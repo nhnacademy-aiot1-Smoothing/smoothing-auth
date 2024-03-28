d03afc7b5364a2768152cc0f0c8462dcc14e165f
nakta-y <nakta.y.biz@gmail.com>
2024-03-28 18:38:53 +0900

    [Fix] : BCryptPasswordEncoder import 수정

--- src/main/java/live/smoothing/auth/config/SecurityConfig.java
+++ src/main/java/live/smoothing/auth/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
