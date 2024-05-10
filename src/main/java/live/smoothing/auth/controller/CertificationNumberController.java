828c461c8fc050314be60cf33004b8296e1c5a98
Jiyoon <bystar1687@nate.com>
2024-05-10 09:15:22 +0900

    [Refactor]: @CrossOrigin 어노테이션 제거

--- src/main/java/live/smoothing/auth/controller/CertificationNumberController.java
+++ src/main/java/live/smoothing/auth/controller/CertificationNumberController.java
@@ -19,7 +19,6 @@ import java.security.NoSuchAlgorithmException;
  * @author 김지윤
  */
 @RestController
-@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class CertificationNumberController {
